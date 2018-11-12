19551c65eb08f561406cc0f4856a3aabfed6e1ee
k-jasinska <44068160+k-jasinska@users.noreply.github.com>
2018-11-12 19:36:34 +0100

    Added views

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## [1.0.0] - 2018-11-12
+### Added
+- views:
+* MainPage.xaml
+* ClientList.xaml
+* AddEntranceToCustomer.xaml
+* ViewDetails.xaml
+* EntranceList.xaml
+* AppStyles.xaml
---
+++ GymSystem.App/ViewModels/AddClientViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystem.App.ViewModels
+{
+    public class AddClientViewModel
+    {
+
+        public List<string> EnteranceTypes { get; set; } = new List<string>() { "Normal", "premium" };
+
+        private string _EnteranceTypeSelected;
+
+        public string EnteranceTypeSelected
+        {
+            get { return _EnteranceTypeSelected; }
+            set { _EnteranceTypeSelected = value; }
+        }
+
+    }
+}
