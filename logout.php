e5167aeef6819e95816fdb4374fef2779dab3dca
Sebastian Jankowski <38406394+sebastian-j@users.noreply.github.com>
2019-01-08 17:40:56 +0100

    undo previous incorrect commit

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-    unset($_SESSION['zalogowany']);
-    session_destroy();
-    header("Location: index.php?error=1");
-
-?>
--- script.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector(".burger");
-const activeElement = document.querySelectorAll(".active");
-
-burger.addEventListener("click", function () {
-    for (let i = 0; i < activeElement.length; i++) {
-        activeElement[i].classList.toggle("show")
-    }
-})
